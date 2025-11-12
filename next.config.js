40f47c0d9d9b5b94dd10bce434d6f0488f83970d
Mark Paine <mark.paine@houseofdoge.com>
2025-11-13 12:58:58 +1300

    fix: use dynamic imports for ESM packages (remark) and configure transpilePackages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true
+  trailingSlash: true,
+  transpilePackages: ['remark', 'remark-html']
 };
 
 module.exports = nextConfig;
